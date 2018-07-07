72a056ec84de1a90fcad67769aceced0aec446ed
Thành Trang <trangcongthanh91@gmail.com>
2018-07-07 15:30:10 +0700

    Flex support responsive

--- src/Flex.js
+++ src/Flex.js
@@ -1,21 +1,68 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Base, flex } from './helpers'
+import Base, { is, isExist, responsive } from './Base'
+
+const properties = {
+  flex: 'flex',
+  flexDirection: 'flex-direction',
+  flexFlow: 'flex-flow',
+  flexWrap: 'flex-wrap',
+  flexBasis: 'flex-basis',
+  flexGrow: 'flex-grow',
+  flexShrink: 'flex-shrink',
+  alignItems: 'align-items',
+  alignContent: 'align-content',
+  alignSelf: 'align-self',
+  justifyContent: 'justify-content',
+  justifySelf: 'justify-self',
+  order: 'order',
+}
 
 const StyledFlex = styled(Base)`
-  display: ${props => (props.display ? props.display : 'flex')};
-  flex-direction: ${props => (props.direction ? props.direction : 'row')};
-  flex-wrap: ${props => (props.wrap ? props.wrap : 'nowrap')};
-  justify-content: ${props => (props.justifyContent ? props.justifyContent : 'flex-start')};
-  align-items: ${props => (props.alignItems ? props.alignItems : 'stretch')};
-  align-content: ${props => (props.alignContent ? props.alignContent : 'stretch')};
-  align-self: ${props => (props.alignSelf ? props.alignSelf : 'auto')};
-  flex: ${flex};
+  display: flex;
+  ${is('inline')`
+    display: inline-flex;
+  `};
+  ${is('column')`
+    flex-direction: column;
+  `};
+  ${isExist(properties)};
+  ${responsive(properties)};
 `
 
 const Flex = ({ as, ...restProps }) => {
   const Component = as && as !== 'div' ? StyledFlex.withComponent(as) : StyledFlex
   return <Component {...restProps} />
+}
+
+Flex.propTypes = {
+  /**
+   * as
+   */
+  as: PropTypes.oneOfType([PropTypes.instanceOf(Element), PropTypes.string]),
+  direction: PropTypes.oneOf(['column', 'row']),
+  /**
+   * If it's number, the unit will be px.
+   * margin will override all of shorthand.
+   */
+  margin: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  marginVertical: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  marginHorizontal: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  marginTop: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  marginRight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  marginBottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  marginLeft: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * If it's number, the unit will be px. padding will override all of shorthand.
+   */
+  padding: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  paddingVertical: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  paddingHorizontal: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  paddingTop: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  paddingRight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  paddingBottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  paddingLeft: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 }
 
 export default Flex
