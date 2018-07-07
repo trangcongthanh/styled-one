7eb4ab74efb99335b913c3f81a008033564c099e
Thành Trang <trangcongthanh91@gmail.com>
2018-07-07 15:30:26 +0700

    Grid support responsive

--- src/Grid.js
+++ src/Grid.js
@@ -1,46 +1,67 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Base, gridTemplate, gridGap, gridResponse } from './helpers'
+import Base, { is, isExist, responsive } from './Base'
+
+const properties = {
+  gridTemplateRows: 'grid-template-rows',
+  gridTemplateColumns: 'grid-template-columns',
+  gridTemplate: 'grid-template',
+  gridRowGap: 'grid-row-gap',
+  gridColumnGap: 'grid-column-gap',
+  gridGap: 'grid-gap',
+  gridAreas: 'grid-template-areas',
+  gridTemplateAreas: 'grid-template-areas',
+
+  justifyItems: 'justify-items',
+  alignItems: 'align-items',
+  placeItems: 'place-items',
+
+  justifyContent: 'justify-content',
+  alignContent: 'align-content',
+  placeContent: 'place-content',
+
+  gridAutoRows: 'grid-auto-rows',
+  gridAutoColumns: 'grid-auto-columns',
+  gridAutoFlow: 'grid-auto-flow',
+  gridFlow: 'grid-auto-flow',
+  grid: 'grid',
+}
+
+const getTemplate = position => props => {
+  switch (position) {
+    case 'row': {
+      if (props.rowHeight && Array.isArray(props['rowHeight'])) {
+        return props['rowHeight'].join(' ')
+      }
+      return `repeat(${props.row || 1}, ${props.rowHeight || '1fr'})`
+    }
+    default: {
+      if (props.columnWidth && Array.isArray(props['columnWidth'])) {
+        return props['columnWidth'].join(' ')
+      }
+      return `repeat(${props.column || 1}, ${props.columnWidth || '1fr'})`
+    }
+  }
+}
 
 const StyledGrid = styled(Base)`
   display: grid;
-  grid-template: ${props => `${gridTemplate(props, 'row')} / ${gridTemplate(props, 'column')}`};
-  grid-gap: ${gridGap};
-  grid-template-areas: ${props => (props.gridAreas ? props.gridAreas : 'none')};
-  place-items: ${props =>
-    props.placeItems ? props.placeItems : `${props.alignItems || 'stretch'} ${props.justifyItems || 'stretch'}`};
-  place-content: ${props =>
-    props.placeContent
-      ? props.placeContent
-      : `${props.alignContent || 'start'} ${props.justifyContent || 'space-around'}`};
-  ${props => props.responsive && gridResponse(props)};
+  ${is('inline')`
+    display: inline-grid;
+  `};
+  ${is('row')`
+    grid-template-rows: ${getTemplate('row')};
+  `};
+  ${is('column')`
+    grid-template-columns: ${getTemplate('column')};
+  `};
+  ${isExist(properties)};
+  ${responsive(properties)};
 `
-
-const StyledGridItem = styled(Base)`
-  grid-area: ${props =>
-    props.gridArea
-      ? props.gridArea
-      : `
-        ${props.rowStart || 'auto'} /
-        ${props.columnStart || 'auto'} /
-        ${props.rowEnd || 'auto'} /
-        ${props.columnEnd || 'auto'}
-      `}
-    };
-  place-self: ${props =>
-    props.placeSelf ? props.placeSelf : `${props.alignSelf || 'stretch'} ${props.justifySelf || 'stretch'}`};
-`
-
-const GridItem = ({ as, ...restProps }) => {
-  const Component = as && as !== 'div' ? StyledGridItem.withComponent(as) : StyledGridItem
-  return <Component {...restProps} />
-}
 
 const Grid = ({ as, ...restProps }) => {
   const Component = as && as !== 'div' ? StyledGrid.withComponent(as) : StyledGrid
   return <Component {...restProps} />
 }
-
-Grid.Item = GridItem
 
 export default Grid
