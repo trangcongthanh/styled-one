53c51d8ff87378b8e4805c8b25b91b4e1a9f4067
Thành Trang <trangcongthanh91@gmail.com>
2018-07-12 10:36:00 +0700

    Support type number for config Grid

--- src/Grid.js
+++ src/Grid.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components'
-import One, { is } from './One'
+import One, { is, addUnit } from './One'
 
 const getTemplate = position => props => {
   switch (position) {
     case 'row': {
       if (props.rowHeight && Array.isArray(props['rowHeight'])) {
-        return props['rowHeight'].join(' ')
+        return props['rowHeight'].map(value => addUnit(value)).join(' ')
       }
-      return `repeat(${props.row || 1}, ${props.rowHeight || '1fr'})`
+      return `repeat(${props.row || 1}, ${addUnit(props.rowHeight) || '1fr'})`
     }
     default: {
       if (props.columnWidth && Array.isArray(props['columnWidth'])) {
-        return props['columnWidth'].join(' ')
+        return props['columnWidth'].map(value => addUnit(value)).join(' ')
       }
-      return `repeat(${props.column || 1}, ${props.columnWidth || '1fr'})`
+      return `repeat(${props.column || 1}, ${addUnit(props.columnWidth) || '1fr'})`
     }
   }
 }
